3105da6efcd37a6763bc3174ab7d9aab0161e552
CarrotNinja <r-sherwani@outlook.com>
2023-07-18 17:32:52 -0400

    Progress made on sophisticated player controller

---
+++ Cadence/Cadence/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using Unity.VisualScripting;
+
+
+namespace Controller
+{
+    public class PlayerController : MonoBehaviour, IPlayerController
+    {
+        public Vector3 Velocity { get; private set; }
+        public FrameInput Input { get; private set; }
+        public bool JumpingThisFrame { get; private set; }
+        public bool LandingThisFrame { get; private set; }
+        public Vector3 RawMovement { get; private set; }
+        public bool Grounded => _colDown;
+
+        private Vector3 _lastPosition;
+        private float _currentHorizontalSpeed, _currentVerticalSpeed;
+
+       
+        private bool _active;
+        void Awake() => Invoke(nameof(Activate), 0.5f);
+        void Activate() => _active = true;
+
+        private void Update()
+        {
+            if (!_active) return;
+            // Calculate velocity
+            Velocity = (transform.position - _lastPosition) / Time.deltaTime;
+            _lastPosition = transform.position;
+
+            GatherInput();
+            RunCollisionChecks();
+
+            CalculateWalk(); // Horizontal movement
+            CalculateJumpApex(); // Affects fall speed, so calculate before gravity
+            CalculateGravity(); // Vertical movement
+            CalculateJump(); // Possibly overrides vertical
+
+            MoveCharacter(); // Actually perform the axis movement
+
+
+        }
+        #region Gather Input
+        private void GatherInput()
+        {
+            Input = new FrameInput
+            {
+                JumpDown = UnityEngine.Input.GetButtonDown("Jump"),
+                JumpUp = UnityEngine.Input.GetButtonUp("Jump"),
+                X = UnityEngine.Input.GetAxisRaw("Horizontal")
+            };
+            if (Input.JumpDown)
+            {
+                _lastJumpPressed = Time.time;
+            }
+        }
+        #endregion
+
+        #region Collisions
+        [Header("COLLISION")][SerializeField] private Bounds _characterBounds;
+        [SerializeField] private LayerMask _groundLayer;
+        [SerializeField] private int _detectorCount = 3;
+        [SerializeField] private float _detectionRayLength = 0.1f;
+        [SerializeField][Range(0.1f, 0.3f)] private float _rayBuffer = 0.1f; // Prevents side detectors hitting the ground
+
+        private RayRange _raysUp, _raysRight, _raysDown, _raysLeft;
+        private bool _colUp, _colRight, _colDown, _colLeft;
+
+        private float _timeLeftGrounded;
+        
+        // We use these raycast checks for pre-collision information
+        private void RunCollisionChecks()
+        {
+            // Generate ray ranges. 
+            CalculateRayRanged();
+
+            // Ground
+            LandingThisFrame = false;
+            var groundedCheck = RunDetection(_raysDown);
+            if (_colDown && !groundedCheck) _timeLeftGrounded = Time.time; // Only trigger when first leaving
+            else if (!_colDown && groundedCheck)
+            {
+                _coyoteUsable = true; // Only trigger when first touching
+                LandingThisFrame = true;
+            }
+
+            _colDown = groundedCheck;
+
+            // The rest
+            _colUp = RunDetection(_raysUp);
+            _colLeft = RunDetection(_raysLeft);
+            _colRight = RunDetection(_raysRight);
+
+            bool RunDetection(RayRange range)
+            {
+                return EvaluateRayPositions(range).Any(point => Physics2D.Raycast(point, range.Dir, _detectionRayLength, _groundLayer));
+            }
+        }
+        private void CalculateRayRanged()
+        {
+            
+            var b = new Bounds(transform.position + _characterBounds.center, _characterBounds.size);
+
+            _raysDown = new RayRange(b.min.x + _rayBuffer, b.min.y, b.max.x - _rayBuffer, b.min.y, Vector2.down);
+            _raysUp = new RayRange(b.min.x + _rayBuffer, b.max.y, b.max.x - _rayBuffer, b.max.y, Vector2.up);
+            _raysLeft = new RayRange(b.min.x, b.min.y + _rayBuffer, b.min.x, b.max.y - _rayBuffer, Vector2.left);
+            _raysRight = new RayRange(b.max.x, b.min.y + _rayBuffer, b.max.x, b.max.y - _rayBuffer, Vector2.right);
+        }
+        private IEnumerable<Vector2> EvaluateRayPositions(RayRange range)
+        {
+            for (var i = 0; i < _detectorCount; i++)
+            {
+                var t = (float)i / (_detectorCount - 1);
+                yield return Vector2.Lerp(range.Start, range.End, t);
+            }
+        }
+        #endregion
+    }
+
+}
