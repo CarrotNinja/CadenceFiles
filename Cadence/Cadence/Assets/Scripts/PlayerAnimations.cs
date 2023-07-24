2d9ef1bb1e7a1d30b13dce828e81d8b15ce5dc9c
CarrotNinja <r-sherwani@outlook.com>
2023-07-23 23:20:46 -0400

    fixes

--- Cadence/Cadence/Assets/Scripts/PlayerAnimations.cs
+++ Cadence/Cadence/Assets/Scripts/PlayerAnimations.cs
@@ -1,6 +1,7 @@
 using Controller;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerAnimations : MonoBehaviour
@@ -8,7 +9,18 @@ public class PlayerAnimations : MonoBehaviour
     [SerializeField] private Animator _anim;
     [SerializeField] private LayerMask _groundMask;
 
+    private const string PLAYERIDLE = "PlayerIdle_";
+    private const string PLAYERRUN = "PlayerRun_";
+    private const string PLAYERHURT = "PlayerHurt_";
+    private const string PLAYERLANDING = "PlayerLanding_";
+    private const string PLAYERLIFTOFF = "PlayerLiftoff_";
+    private const string PLAYERFLOAT = "PlayerFloat_";
+
+    private string currentState;
+    private char currentHealth = 'H';
+
     private IPlayerController _player;
+    private Vector2 movement;
     void Awake() => _player = GetComponentInParent<IPlayerController>();
 
     // Update is called once per frame
@@ -16,11 +28,43 @@ public class PlayerAnimations : MonoBehaviour
     {
         if (_player == null) return;
         if (_player.Input.X != 0) transform.localScale = new Vector3(_player.Input.X > 0 ? 1 : -1, 1, 1);
+        movement = _player.RawMovement;
 
-        _anim.SetBool("Jumping", _player.JumpingThisFrame);
-        _anim.SetFloat("VelocityX", Mathf.Abs(_player.RawMovement.x));
-        _anim.SetFloat("VelocityY", _player.RawMovement.y);
-        _anim.SetBool("Grounded", _player.Grounded);
+        if (currentState == PLAYERFLOAT && _player.Grounded)
+        {
+            ChangeAnimationState(PLAYERLANDING);
+        }
+        else if ((currentState == PLAYERRUN || currentState == PLAYERIDLE) && !_player.Grounded)
+        {
+            ChangeAnimationState(PLAYERLIFTOFF);
+        }
+        else if (Mathf.Abs(movement.x) < 0.1 && _player.Grounded)
+        {
+            ChangeAnimationState(PLAYERIDLE);
+        }
+        else if (_player.Grounded)
+        {
+            ChangeAnimationState(PLAYERRUN);
+        }
+        else if (!_player.Grounded)
+        {
+            ChangeAnimationState(PLAYERFLOAT);
+        }
+
     }
 
+    bool IsAnimationFinished()
+    {
+        AnimatorStateInfo stateInfo = _anim.GetCurrentAnimatorStateInfo(0);
+        return stateInfo.normalizedTime >= 1.0f;
+    }
+
+    void ChangeAnimationState(string newState)
+    {
+        if (newState == currentState) return;
+        if (!IsAnimationFinished() && (currentState == PLAYERLIFTOFF || currentState == PLAYERLANDING)) return;
+        _anim.Play(newState + currentHealth);
+        currentState = newState;
+
+    }
 }
