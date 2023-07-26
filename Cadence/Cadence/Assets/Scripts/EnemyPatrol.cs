57f840af0168915d52bf81326abeb98a04451d09
CarrotNinja <r-sherwani@outlook.com>
2023-07-26 15:00:50 -0400

    Enemy patrol system done

---
+++ Cadence/Cadence/Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public GameObject pointA;
+    public GameObject pointB;
+    public float speed;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Transform currentPoint;
+    private bool facingRight=true;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        currentPoint = pointB.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 point = currentPoint.position - transform.position;
+        if(currentPoint == pointB.transform)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+
+        if(Vector2.Distance(transform.position,currentPoint.position)<0.5f && currentPoint == pointB.transform)
+        {
+            currentPoint = pointA.transform;
+            if (!facingRight)
+            {
+                flip();
+            }
+        }
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+        {
+            currentPoint = pointB.transform;
+            if (facingRight)
+            {
+                flip();
+            }
+        }
+    }
+    private void flip()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale= localScale;
+        facingRight=!facingRight;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(pointA.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(pointB.transform.position, 0.5f);
+        Gizmos.DrawLine(pointA.transform.position,pointB.transform.position);
+    }
+}
